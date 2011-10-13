130c1f24ab29d99273a03c1f6c98ee21d70ed50b
Derrick <dellerbie@gmail.com>
2011-10-12 18:46:16 -0700

    Added a spec to not download and save the restaurant page if it already exists

--- spec/categories_downloader_spec.rb
+++ spec/categories_downloader_spec.rb
@@ -4,10 +4,19 @@ PLACES_ROOT = File.expand_path('..')
 require 'places'
 
 describe Places::Downloaders::Categories do 
-  it "should download and save restaurants page" do
+  it "should download and save the restaurants index page" do
     Places::Downloaders::Categories.download_and_save_restaurants_page
     File.should exist(Places::Downloaders::Categories::RESTAURANTS_PAGE)
     File.size(Places::Downloaders::Categories::RESTAURANTS_PAGE).should be > 0
+  end
+  
+  it "should not download and save the restaurants index page if it already exists" do
+    Places::Downloaders::Categories.download_and_save_restaurants_page
+    restaurants_page = Places::Downloaders::Categories::RESTAURANTS_PAGE
+    first_time = File.mtime(restaurants_page)
+    Places::Downloaders::Categories.download_and_save_restaurants_page
+    second_time = File.mtime(restaurants_page)
+    first_time.should == second_time
   end
 
   it "should write categories to yaml" do 
@@ -18,4 +27,5 @@ describe Places::Downloaders::Categories do
   end
   
   it "should download and save the top40 restaurants by category pages"
+  it "should not download a top40 restaurant category page if it already exists"
 end
