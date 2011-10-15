5e0d9b9c038a20b92868cbce997ce9e4cc52f81b
Derrick Ellerbie <dellerbie@gmail.com>
2011-10-15 13:23:53 -0700

    Removed the old source files

--- get_image_pages.rb
+++
@@ -1,9 +0,0 @@
-$:.unshift(File.join(File.dirname(__FILE__), 'lib'))
-
-require 'yaml'
-require 'places'
-
-PLACES_ROOT = File.expand_path(File.dirname(__FILE__))
-ENV['http_proxy'] = "http://laocache:8080/"
-
-BusinessPopulator2.business_image_pages
