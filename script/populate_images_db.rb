de601f4d56d35d70a2cf9c247ce30de5c476eb72
Derrick Ellerbie <dellerbie@gmail.com>
2014-01-29 12:31:00 -0800

    Use socksify ruby

--- script/populate_images_db.rb
+++ script/populate_images_db.rb
@@ -5,6 +5,16 @@ RESIZE_IMAGES_ROOT = File.join(PLACES_ROOT, 'resized_images')
 
 require 'places'
 
-Places::ImageResizer.resize_images
-Places::ImageResizer.load_images_with_businesses
+Places::CategoryBuilder.download_and_save_restaurants_page
+# Places::CategoryBuilder.download_and_save_top_40_restaurants_by_category_pages
+# 
+# Places::BusinessBuilder.businesses_from_category_pages
+# Places::BusinessBuilder.download_business_pages
+# Places::BusinessBuilder.download_business_image_pages
+# 
+# Places::ImageBuilder.write_images_to_yaml
+# Places::ImageBuilder.download_images_from_yaml
+
+# Places::ImageResizer.resize_images
+# Places::ImageResizer.load_images_with_businesses
 # Places::ImagePopulator.populate!
